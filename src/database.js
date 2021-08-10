fd4cab139b488090f50f2545e8ebc33af9adb252
Lucas <lucas_tavares@id.uff.br>
2021-08-10 17:26:50 -0300

    dotenv config

--- src/database.js
+++ src/database.js
@@ -1,12 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import pg from "pg";
 const { Pool } = pg;
 
 const connection = new Pool({
-    user: 'postgres',
-    password: '123456',
-    host: 'localhost',
-    port: 5432,
-    database: 'mywallet'
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized:false
+    }
 });
 
 export default connection;
